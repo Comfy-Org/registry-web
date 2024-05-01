6d1cdb6d8e51db3741f0c1a2bd6c27c8297ab10a
Robin Huang <robin.j.huang@gmail.com>
2024-04-30 20:59:43 -0700

    Add firebase jwt token to auth.

--- src/api/mutator/axios-instance.ts
+++ src/api/mutator/axios-instance.ts
@@ -1,7 +1,20 @@
 import Axios, { AxiosRequestConfig } from 'axios'
+import { getAuth } from 'firebase/auth'
 
 const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL
 export const AXIOS_INSTANCE = Axios.create({ baseURL: BACKEND_URL }) // use your own URL here or environment variable
+
+// Add an interceptor to attach the Firebase JWT token to every request
+AXIOS_INSTANCE.interceptors.request.use(async (config) => {
+    const auth = getAuth()
+    const user = auth.currentUser
+
+    if (user) {
+        const token = await user.getIdToken()
+        config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+})
 
 export const customInstance = <T>(
     config: AxiosRequestConfig,
