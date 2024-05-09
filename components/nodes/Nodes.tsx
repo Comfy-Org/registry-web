0ac3f58adcd09113eca596df5e1460c4cf010661
Paul <paul@sand-stone-tech.com>
2024-05-09 14:10:53 +0500

    set type marker

--- components/nodes/Nodes.tsx
+++ components/nodes/Nodes.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
 import GenericHeader from '../common/GenericHeader'
 import NodesCard from './NodesCard'
-import { NodeEditPublishModal } from './NodeEditPublishModal';
+import { NodeEditPublishModal } from './NodeEditPublishModal'
 
-import { NodeEditMarkerModal } from './NodeEditMarkerModal';
+import { NodeEditMarkerModal } from './NodeEditMarkerModal'
 export const NodesData = [
     {
         id: '1',
@@ -17,19 +17,19 @@ export const NodesData = [
         downloads: '977k ',
     },
 ]
-const repeatedNodes = Array(6).fill(NodesData[0]);
-const publishingNodes = Array(3).fill(NodesData[0]);
+const repeatedNodes = Array(6).fill(NodesData[0])
+const publishingNodes = Array(3).fill(NodesData[0])
 const Nodes: React.FC = () => {
-    const [openEditModal, setOpenEditModal] = useState(false) 
-    const [openEditMarkerModal, setOpenEditMarkerModal] = useState(false) 
-    
+    const [openEditModal, setOpenEditModal] = useState(false)
+    const [openEditMarkerModal, setOpenEditMarkerModal] = useState(false)
+
     const onCloseEditModal = () => {
         setOpenEditModal(false)
     }
     const handleEditButtonClick = () => {
         setOpenEditModal(true)
     }
-      
+
     const onCloseEditMarkerModal = () => {
         setOpenEditMarkerModal(false)
     }
@@ -45,6 +45,7 @@ const Nodes: React.FC = () => {
                     buttonText="New Publisher"
                     buttonLink=""
                     showIcon={true}
+                    buttonColor=""
                 />
             </div>
             {/* <FilterRegistry /> */}
@@ -53,31 +54,31 @@ const Nodes: React.FC = () => {
                     <h1 className="text-lg font-bold leading-tight tracking-tight text-white sm:text-2xl">
                         NodeMaker
                     </h1>
-                    
-                    <div   onClick={handleEditMarkerButtonClick}>
-                    <svg
-                        className="w-[20px] h-[17px] text-white ml-2"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="10"
-                        height="10"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                    >
-                        <path
-                            stroke="currentColor"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M10.779 17.779 4.36 19.918 6.5 13.5m4.279 4.279 8.364-8.643a3.027 3.027 0 0 0-2.14-5.165 3.03 3.03 0 0 0-2.14.886L6.5 13.5m4.279 4.279L6.499 13.5m2.14 2.14 6.213-6.504M12.75 7.04 17 11.28"
-                        />
-                    </svg>
+
+                    <div onClick={handleEditMarkerButtonClick}>
+                        <svg
+                            className="w-[20px] h-[17px] text-white ml-2"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="10"
+                            height="10"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                        >
+                            <path
+                                stroke="currentColor"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M10.779 17.779 4.36 19.918 6.5 13.5m4.279 4.279 8.364-8.643a3.027 3.027 0 0 0-2.14-5.165 3.03 3.03 0 0 0-2.14.886L6.5 13.5m4.279 4.279L6.499 13.5m2.14 2.14 6.213-6.504M12.75 7.04 17 11.28"
+                            />
+                        </svg>
                     </div>
                 </div>
 
                 <div className="grid gap-4 pt-8 mb-6 lg:mb-5 md:grid-cols-2 lg:grid-cols-3">
                     {repeatedNodes.map((member, index) => (
-                        <NodesCard key={index} {...member} title={"marker"}/>
+                        <NodesCard key={index} {...member} type="marker" />
                     ))}
                 </div>
             </div>
@@ -86,31 +87,31 @@ const Nodes: React.FC = () => {
                     <h1 className="text-lg font-bold leading-tight tracking-tight text-white sm:text-2xl">
                         Publishing Org
                     </h1>
-                  
-                    <div   onClick={handleEditButtonClick}>
-                    <svg
-                        className="w-[20px] h-[17px] text-white ml-2"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="10"
-                        height="10"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                    >
-                        <path
-                            stroke="currentColor"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M10.779 17.779 4.36 19.918 6.5 13.5m4.279 4.279 8.364-8.643a3.027 3.027 0 0 0-2.14-5.165 3.03 3.03 0 0 0-2.14.886L6.5 13.5m4.279 4.279L6.499 13.5m2.14 2.14 6.213-6.504M12.75 7.04 17 11.28"
-                        />
-                    </svg>
+
+                    <div onClick={handleEditButtonClick}>
+                        <svg
+                            className="w-[20px] h-[17px] text-white ml-2"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="10"
+                            height="10"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                        >
+                            <path
+                                stroke="currentColor"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M10.779 17.779 4.36 19.918 6.5 13.5m4.279 4.279 8.364-8.643a3.027 3.027 0 0 0-2.14-5.165 3.03 3.03 0 0 0-2.14.886L6.5 13.5m4.279 4.279L6.499 13.5m2.14 2.14 6.213-6.504M12.75 7.04 17 11.28"
+                            />
+                        </svg>
                     </div>
                 </div>
 
                 <div className="grid gap-4 pt-8 mb-6 lg:mb-5 md:grid-cols-2 lg:grid-cols-3">
                     {publishingNodes.map((member, index) => (
-                        <NodesCard key={index} {...member} title={"publisher"} />
+                        <NodesCard key={index} {...member} type="publisher" />
                     ))}
                 </div>
             </div>
@@ -118,11 +119,10 @@ const Nodes: React.FC = () => {
                 openModal={openEditModal}
                 onCloseModal={onCloseEditModal}
             />
-             <NodeEditMarkerModal
+            <NodeEditMarkerModal
                 openModal={openEditMarkerModal}
                 onCloseModal={onCloseEditMarkerModal}
             />
-            
         </section>
     )
 }
