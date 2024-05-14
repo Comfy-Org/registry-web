47f8ad50a856ec97a9717a019e00f8f8e783e05a
Robin Huang <robin.j.huang@gmail.com>
2024-05-13 22:52:06 -0700

    Rename firebase config variables to match deployed variables.

--- src/api/mutator/axios-instance.ts
+++ src/api/mutator/axios-instance.ts
@@ -4,9 +4,9 @@ import { initializeApp, getApps, getApp } from 'firebase/app'
 const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_DEV_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_DEV_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_DEV_PROJECT_ID,
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
 }
 getApps().length === 0 ? initializeApp(firebaseConfig) : getApp()
 export const AXIOS_INSTANCE = Axios.create({ baseURL: BACKEND_URL }) // use your own URL here or environment variable
