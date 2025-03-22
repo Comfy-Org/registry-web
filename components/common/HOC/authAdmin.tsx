eae3cd45960da5d24cc482bbf71750771e5003a6
snomiao <snomiao@gmail.com>
2025-03-22 14:59:08 +0000

    fix(sno-admin-redir): Show 403 when user === undefined
    Show 403 when user === undefined

--- components/common/HOC/authAdmin.tsx
+++ components/common/HOC/authAdmin.tsx
@@ -3,24 +3,35 @@ import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 import { useGetUser } from 'src/api/generated'
 
-// this HOC component should be used in page level, since h-[50vh] in loading spinner settle
+/** this HOC component should be used in page level, since h-[50vh] in loading spinner settle */
 const withAdmin = (WrappedComponent) => {
     const HOC = (props: JSX.IntrinsicAttributes) => {
         const router = useRouter()
         const { data: user, isLoading } = useGetUser()
-
         useEffect(() => {
             if (!isLoading && !user?.isAdmin) {
                 router.push('/')
             }
         }, [user, router, isLoading])
+
         if (isLoading)
             return (
                 <div className="flex-grow flex justify-center items-center h-[50vh]">
                     <Spinner />
                 </div>
             )
-        return <WrappedComponent {...props} />
+
+        if (user?.isAdmin) {
+            return <WrappedComponent {...props} />
+        }
+
+        // Show 403 when user === undefined
+        return (
+            <div className="text-white dark:text-white">
+                403 Forbidden: You have no permission to this page. Redirecting
+                to home page.
+            </div>
+        )
     }
 
     if (WrappedComponent.getInitialProps) {
