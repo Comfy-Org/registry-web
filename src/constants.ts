830edd72c01885e7a1270675b644d1ba11d2b0ad
snomiao <snomiao@gmail.com>
2025-03-23 00:12:13 +0800

    Update src/constants.ts
    Co-authored-by: Robin Huang <robin.j.huang@gmail.com>

--- src/constants.ts
+++ src/constants.ts
@@ -6,4 +6,4 @@ export const INSTANT_SEARCH_HIERARCHICAL_ATTRIBUTES = [
 ]
 
 export const UNCLAIMED_ADMIN_PUBLISHER_ID =
-    'admin-11338bd3-f081-43cf-b3f9-295c829826f7' // copy form https://github.com/Comfy-Org/comfy-api/blob/main/db/publisher.go#L13
+    'admin-11338bd3-f081-43cf-b3f9-295c829826f7' // copy from https://github.com/Comfy-Org/comfy-api/blob/main/db/publisher.go#L13
