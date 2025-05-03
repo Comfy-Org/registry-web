e66cd3e941e853ca52d6668c3da6e7badac8f534
snomiao <snomiao@gmail.com>
2025-05-03 10:30:55 +0000

    fix(sno-tidy-auth-hoc): improve hoc logic for readability a
    improve hoc logic for readability and give better error response

--- components/common/HOC/authAdmin.tsx
+++ components/common/HOC/authAdmin.tsx
@@ -3,14 +3,21 @@ import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 import { useGetUser } from 'src/api/generated'
 
-/** this HOC component should be used in page level, since h-[50vh] in loading spinner settle */
+/**
+ * Admin dashboard HOC
+ * case User is not logged in: show 401, and then redirect to login page
+ * case User is logged in but not admin: show 403 page
+ * case User is logged in and admin: show the page
+ *
+ * this HOC component should be used in page level, since h-[50vh] in loading spinner settle
+ */
 const withAdmin = (WrappedComponent) => {
     const HOC = (props: JSX.IntrinsicAttributes) => {
         const router = useRouter()
         const { data: user, isLoading } = useGetUser()
         useEffect(() => {
-            if (!isLoading && !user?.isAdmin) {
-                router.push('/')
+            if (!isLoading && !user) {
+                router.push('/auth/login')
             }
         }, [user, router, isLoading])
 
@@ -21,17 +28,23 @@ const withAdmin = (WrappedComponent) => {
                 </div>
             )
 
-        if (user?.isAdmin) {
-            return <WrappedComponent {...props} />
+        if (!user) {
+            return (
+                <div className="text-white dark:text-white">
+                    401 Unauthorized: You have no permission to this page.
+                </div>
+            )
         }
 
-        // Show 403 when user === undefined and user.isAdmin === false
-        return (
-            <div className="text-white dark:text-white">
-                403 Forbidden: You have no permission to this page. Redirecting
-                to home page.
-            </div>
-        )
+        if (!user.isAdmin) {
+            return (
+                <div className="text-white dark:text-white">
+                    403 Forbidden: You have no permission to this page.
+                </div>
+            )
+        }
+
+        return <WrappedComponent {...props} />
     }
 
     if (WrappedComponent.getInitialProps) {
