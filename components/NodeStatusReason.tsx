b2864975dd1a5e23db9e6324d09db1a10b9ff036
snomiao <7323030+snomiao@users.noreply.github.com>
2025-05-05 13:52:34 +0000

    format: Apply prettier --fix changes

--- components/NodeStatusReason.tsx
+++ components/NodeStatusReason.tsx
@@ -104,7 +104,7 @@ export function NodeStatusReason({ node_id, status_reason }: NodeVersion) {
         statusReasonJson?.map?.(({ error_type, type, issue_type, ...e }) => ({
             issue_type: issue_type || error_type || type,
             line_number: e.line_number || e.line || -1,
-            file_path: e.file_path || e.filename || e.path || e.file ,
+            file_path: e.file_path || e.filename || e.path || e.file,
             ...e,
         }))
     ).data
