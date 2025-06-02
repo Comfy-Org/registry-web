4c562e53ddd66e09a9c9782e50d448c4736cffb2
snomiao <snomiao@gmail.com>
2025-06-02 14:04:36 +0900

    Revert "feat(NodeDetails): add NodeVersionStatusBadge component to display ve…" (#99)
    This reverts commit d0821aa7e27b3c92fe45556091e0274f88631d7d.

--- components/nodes/NodeVersionStatusBadge.tsx
+++ components/nodes/NodeVersionStatusBadge.tsx
@@ -4,35 +4,19 @@ import { NodeVersionStatus } from 'src/api/generated'
 const NodeVersionStatusBadge: React.FC<{ status?: NodeVersionStatus }> = ({
     status,
 }) => {
-    const statusMap: Record<
-        NodeVersionStatus,
-        { color: string; label: string }
-    > = {
-        [NodeVersionStatus.NodeVersionStatusActive]: {
-            color: 'success',
-            label: 'Live',
-        },
-        [NodeVersionStatus.NodeVersionStatusPending]: {
-            color: 'warning',
-            label: 'Pending Security Review',
-        },
-        [NodeVersionStatus.NodeVersionStatusFlagged]: {
-            color: 'warning',
-            label: 'Pending Security Review',
-        },
-        [NodeVersionStatus.NodeVersionStatusBanned]: {
-            color: 'failure',
-            label: 'Rejected',
-        },
-        [NodeVersionStatus.NodeVersionStatusDeleted]: {
-            color: 'failure',
-            label: 'Deleted',
-        },
+    if (status === NodeVersionStatus.NodeVersionStatusActive) {
+        return <Badge color="success">Live</Badge>
     }
 
-    if (status && statusMap[status]) {
-        const { color, label } = statusMap[status]
-        return <Badge color={color}>{label}</Badge>
+    if (
+        status === NodeVersionStatus.NodeVersionStatusPending ||
+        NodeVersionStatus.NodeVersionStatusFlagged
+    ) {
+        return <Badge color="warning">Pending Security Review</Badge>
+    }
+
+    if (status === NodeVersionStatus.NodeVersionStatusBanned) {
+        return <Badge color="failure">Banned</Badge>
     }
 
     return null
