e06f640938820c424fd6a4d7c5312ca2c3a27aa1
snomiao <snomiao@gmail.com>
2025-06-03 06:03:26 +0000

    fix(AdminCreateNodeFormModal): re-enable toast notifications for better user feedback fix(AdminCreateNodeFormModal): set dismissible prop to false to prevent modal from being dismissed unintentionally

--- components/nodes/AdminCreateNodeFormModal.tsx
+++ components/nodes/AdminCreateNodeFormModal.tsx
@@ -3,8 +3,8 @@ import { AxiosError } from 'axios'
 import { Button, Label, Modal, Textarea, TextInput } from 'flowbite-react'
 import { useRouter } from 'next/router'
 import { useForm } from 'react-hook-form'
-import { toast } from 'react-toastify'
 import { HiPlus } from 'react-icons/hi'
+import { toast } from 'react-toastify'
 import {
     Node,
     useAdminCreateNode,
@@ -113,7 +113,7 @@ export function AdminCreateNodeFormModal({
             popup
             //@ts-ignore
             theme={customThemeTModal}
-            dismissible
+            dismissible={false}
         >
             <Modal.Body className="!bg-gray-800 p-8 md:px-9 md:py-8 rounded-none">
                 <Modal.Header className="!bg-gray-800">
