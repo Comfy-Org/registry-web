1d7f8f32a58abc58617ec9ca7ad02cc55e36f385
snomiao <snomiao@gmail.com>
2025-06-03 02:21:14 +0000

    feat(Logout.tsx): refactor logout functionality into a custom hook for better reusability and separation of concerns fix(Logout.tsx): handle logout errors with toast notifications to improve user experience refactor(ProfileDropdown.tsx): utilize the new useLogout hook for sign-out functionality to streamline code and enhance maintainability

--- components/AuthUI/Logout.tsx
+++ components/AuthUI/Logout.tsx
@@ -1,19 +1,31 @@
 import { useQueryClient } from '@tanstack/react-query'
 import { getAuth } from 'firebase/auth'
 import { Button } from 'flowbite-react'
+import { useEffect } from 'react'
 import { useSignOut } from 'react-firebase-hooks/auth'
+import { toast } from 'react-toastify'
 import app from '../../src/firebase'
 
-const Logout = () => {
+export function useLogout() {
     const auth = getAuth(app)
-    const [signOut, user, loading] = useSignOut(auth)
     const qc = useQueryClient()
-    const onLogout = async () => {
+    const [signOut, loading, error] = useSignOut(auth)
+    const logout = async () => {
+        await signOut()
+        qc.resetQueries() // Reset all queries, which will refetch data on next access
         qc.clear() // Clear the query cache to remove user data
         window.localStorage.clear() // Clear local storage
         window.sessionStorage.clear() // Clear session storage
-        await signOut()
     }
+    useEffect(() => {
+        if (error)
+            toast.error('Logout error:' + String(error?.message || error))
+    }, [error])
+    return [logout, loading, error] as const
+}
+
+const Logout = () => {
+    const [onLogout, loading, error] = useLogout()
     return (
         <div className="flex items-center justify-center min-h-screen ">
             <section className="p-4">
