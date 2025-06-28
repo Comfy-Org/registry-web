c5be22bd35d8da69877a1db4bcc5e567c8f9d925
snomiao <snomiao@gmail.com>
2025-06-28 04:20:27 +0000

    fix(next.config.ts): correct locale code from 'kr' to 'ko' for South Korea to adhere to ISO language standards

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@ const withMDX = mdx({
 const conf: NextConfig = {
     reactStrictMode: true,
     i18n: {
-        locales: ['en', 'zh', 'ja', 'fr', 'es', 'kr'],
+        locales: ['en', 'zh', 'ja', 'fr', 'es', 'ko'],
         defaultLocale: 'en',
     },
 
