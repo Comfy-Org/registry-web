331ce3514e419793f9055c0d23dfcfb87c118e11
snomiao <7323030+snomiao@users.noreply.github.com>
2025-07-20 07:51:55 +0000

    format: Apply prettier --fix changes

--- components/nodes/NodeDetails.tsx
+++ components/nodes/NodeDetails.tsx
@@ -286,15 +286,27 @@ const NodeDetails = () => {
                                         className="text-[18px] pt-2 text-gray-300"
                                         hidden={isUnclaimed}
                                     >
-                                        {node.latest_version ? (
-                                            t('{{publisher}} | v{{version}} {{mostRecentLabel}}', {
-                                                publisher: node.publisher?.id?.replace(/^(?!$)/, '@') || '',
-                                                version: node.latest_version.version,
-                                                mostRecentLabel: t('Most recent version')
-                                            })
-                                        ) : (
-                                            node.publisher?.id?.replace(/^(?!$)/, '@')
-                                        )}
+                                        {node.latest_version
+                                            ? t(
+                                                  '{{publisher}} | v{{version}} {{mostRecentLabel}}',
+                                                  {
+                                                      publisher:
+                                                          node.publisher?.id?.replace(
+                                                              /^(?!$)/,
+                                                              '@'
+                                                          ) || '',
+                                                      version:
+                                                          node.latest_version
+                                                              .version,
+                                                      mostRecentLabel: t(
+                                                          'Most recent version'
+                                                      ),
+                                                  }
+                                              )
+                                            : node.publisher?.id?.replace(
+                                                  /^(?!$)/,
+                                                  '@'
+                                              )}
                                     </p>
                                 </div>
                             </div>
