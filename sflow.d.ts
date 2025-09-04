4207f8c2dcac1791aa9aa10df260fdb9a054a1d8
snomiao <snomiao@gmail.com>
2025-09-04 00:52:13 +0000

    chore: update sflow to 1.24.3
    Update sflow dependency from 1.24.2 to 1.24.3 to get the latest features and fixes.

--- sflow.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'sflow/src/strings' {
-    export const replaces: any
-}
-
-declare module 'sflow' {
-    const sflow: any
-    export default sflow
-    export const pageFlow: any
-}
