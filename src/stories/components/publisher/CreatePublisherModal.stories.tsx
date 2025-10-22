fc6f2efb3194ad7c58de25b0758c7c32d9d87d4a
snomiao <snomiao@gmail.com>
2025-10-22 13:26:51 +0000

    fix: Update Storybook import to use nextjs-vite package
    - Change import from @storybook/react to @storybook/nextjs-vite
    - Resolves linting error about direct renderer package imports

--- src/stories/components/publisher/CreatePublisherModal.stories.tsx
+++ src/stories/components/publisher/CreatePublisherModal.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/nextjs-vite'
 import CreatePublisherModal from '../../../../components/publisher/CreatePublisherModal'
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
@@ -18,7 +18,7 @@ const meta: Meta<typeof CreatePublisherModal> = {
         onSuccess: { action: 'onSuccess' },
     },
     // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-    args: { 
+    args: {
         openModal: true,
         onCloseModal: () => {},
     },
