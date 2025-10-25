6280fc2bf7629c22eee6af247b036141e704c0d6
snomiao <snomiao@gmail.com>
2025-10-25 10:00:53 +0000

    fix: Correct import path in CreatePublisherModal stories
    Update import to use relative path since story file is now co-located
    with the component in components/publisher/ directory after Storybook
    restructure.

--- components/publisher/CreatePublisherModal.stories.tsx
+++ components/publisher/CreatePublisherModal.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/nextjs-vite'
-import CreatePublisherModal from '../../../../components/publisher/CreatePublisherModal'
+import CreatePublisherModal from './CreatePublisherModal'
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta: Meta<typeof CreatePublisherModal> = {
