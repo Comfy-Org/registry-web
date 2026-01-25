4bae204b20c711c2e792650c4054c9192743e95a
Robin Huang <robin.j.huang@gmail.com>
2026-01-24 17:18:27 -0800

    Add google analytics.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -21,7 +21,22 @@ class MyDocument extends Document {
 
     return (
       <Html lang={locale} dir={dir}>
-        <Head />
+        <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-1BJGRF43F4"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-1BJGRF43F4');
+              `,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
