028ec6c87bc33bcf164157c4eee67255ca4fa58d
Dhruv Jindal <dhruv.jindal@networkon.com>
2026-06-09 17:27:01 +0530

    chore: add dynamic canonical tags to auth endpoints to fix duplicate SEO errors

--- pages/auth/logout.tsx
+++ pages/auth/logout.tsx
@@ -1,11 +1,22 @@
-import React from "react";
-import Logout from "../../components/AuthUI/Logout";
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import React from 'react'
+import Logout from '../../components/AuthUI/Logout'
+import { SITE_URL } from '../../src/constants'
 
 const LogoutPage: React.FC = () => {
+  const router = useRouter()
+  const localePrefix =
+    router.locale === router.defaultLocale ? '' : `/${router.locale}`
+  const canonicalUrl = `${SITE_URL}${localePrefix}${router.pathname}`
+
   return (
     <>
+      <Head>
+        <link rel="canonical" href={canonicalUrl} />
+      </Head>
       <Logout />
     </>
-  );
-};
-export default LogoutPage;
+  )
+}
+export default LogoutPage
--- pages/auth/signup.tsx
+++ pages/auth/signup.tsx
@@ -1,11 +1,22 @@
-import React from "react";
-import SignIn from "../../components/AuthUI/AuthUI";
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import React from 'react'
+import SignIn from '../../components/AuthUI/AuthUI'
+import { SITE_URL } from '../../src/constants'
 
 const SignUpPage: React.FC = () => {
+  const router = useRouter()
+  const localePrefix =
+    router.locale === router.defaultLocale ? '' : `/${router.locale}`
+  const canonicalUrl = `${SITE_URL}${localePrefix}${router.pathname}`
+
   return (
     <>
+      <Head>
+        <link rel="canonical" href={canonicalUrl} />
+      </Head>
       <SignIn />
     </>
-  );
-};
-export default SignUpPage;
+  )
+}
+export default SignUpPage
